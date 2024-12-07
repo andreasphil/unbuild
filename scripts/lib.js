cf805839e205ffa51a9687d4ae740070213948ec
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-12-07 08:50:39 +0100

    feat: add util to detect dev mode

--- scripts/lib.js
+++ scripts/lib.js
@@ -14,3 +14,11 @@ const tag = (strings, ...values) => String.raw({ raw: strings }, ...values);
  * allow syntax highlighting and formatting if your editor supports it.
  */
 export const html = tag;
+
+/**
+ * Simple heuristic for detecting if an app is running in development (a.k.a
+ * served from localhost) or production (everything else).
+ */
+export function isDev() {
+  return window.location.hostname === "localhost";
+}
